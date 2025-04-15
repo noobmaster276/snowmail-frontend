0a281756d024f2faeff60f02b2750de781f874de
yash2974 <yashbisht2004@gmail.com>
2025-04-15 20:47:54 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom'],
-    },
-  },
   resolve: {
     alias: {
-      // You can add any necessary path aliases here
-    },
-  },
-});
+      // Add any necessary aliases here
+    }
+  }
+})
